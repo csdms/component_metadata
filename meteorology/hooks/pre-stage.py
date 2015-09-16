b10321148fccf45f6e8b9c955ac9cf6147852842
Mark Piper <mark.piper@colorado.edu>
2015-09-16 17:09:27 -0600

    Allow upload of Time_Series, Grid, and Grid_Sequence files
    TopoFlow input parameters may take a scalar value, or a file containing
    a time series, grid, or grid sequence. This commit sets such a parameter
    to the correct type based on user input.

--- meteorology/hooks/pre-stage.py
+++ meteorology/hooks/pre-stage.py
@@ -9,11 +9,42 @@ from wmt.utils.hook import find_simulation_input_file
 file_list = ['rti_file',
              'aspect_grid_file',
              'slope_grid_file',
-             'P',
              'pixel_file']
 
 
+def assign_parameter_value(env):
+    """Assign the value of a TopoFlow input parameter.
+
+    A subset of TopoFlow input parameters can take a scalar value, or,
+    through an uploaded file, a time series, a grid, or a grid
+    sequence. This function assigns the parameter a scalar value, or
+    the name of a file, based on the user's selection in WMT.
+
+    Parameters
+    ----------
+    env : dict
+      A dict of component parameter values from WMT.
+
+    """
+    for key in env.copy().iterkeys():
+        if key.endswith('_type'):
+            key_root = str(key.rstrip('_type'))
+            if env[key] == 'Scalar':
+                env[key_root] = env[key_root + '_scalar']
+            else:
+                env[key_root] = env[key_root + '_file']
+                file_list.append(key_root)
+
+
 def execute(env):
+    """Perform pre-stage tasks for running a component.
+
+    Parameters
+    ----------
+    env : dict
+      A dict of component parameter values from WMT.
+
+    """
     env['n_steps'] = int(round(float(env['run_duration']) / float(env['dt'])))
     env['save_grid_dt'] = float(env['dt'])
     env['save_pixels_dt'] = float(env['dt'])
@@ -26,7 +57,7 @@ def execute(env):
         file_list.remove('pixel_file')
         env['pixel_file'] = '_outlets.txt'
 
-    env['P'] = env['case_prefix'] + '_rain_rates.txt'
+    assign_parameter_value(env)
 
     # Default files common to all TopoFlow components are stored with the
     # topoflow component metadata.
