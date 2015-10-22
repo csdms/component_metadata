ee127cb4d83aaf4aa3aec17d95a00d356d2aa318
Mark Piper <mark.piper@colorado.edu>
2015-10-22 11:20:41 -0600

    Remove non-TopoFlow components from hydrology branch

--- hydrotrend/hooks/pre-stage.py
+++
@@ -1,8 +0,0 @@
-
-def execute(env):
-    from math import ceil
-
-
-    duration_in_years = ceil(float(env['run_duration']) / 365.)
-    #duration_in_years = float(env['run_duration']) / 365.
-    env['run_duration'] = str(int(duration_in_years))
